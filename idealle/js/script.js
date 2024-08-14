fd93ccb9689460947ed2ef4b6047782c907375f5
Guilherme Brito Souza Santos <157852429+guidev1911@users.noreply.github.com>
2024-08-14 17:59:47 -0300

    Add files via upload

--- idealle/js/script.js
+++ idealle/js/script.js
@@ -98,3 +98,32 @@ document.addEventListener("DOMContentLoaded", function() {
     // Verifica o scroll ao carregar a página
     onScroll();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.getElementById('toggle-class');
+    const submenu = document.getElementById('submenu');
+
+    menuButton.addEventListener('click', function() {
+        if (submenu.style.display === 'block') {
+            submenu.style.display = 'none';
+        } else {
+            submenu.style.display = 'block';
+        }
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.getElementById('menu-toggle');
+    const submenu = document.getElementById('submenu');
+
+    menuButton.addEventListener('click', function() {
+        if (submenu.style.transform === 'translateY(0%)') {
+            submenu.style.transform = 'translateY(-100%)';
+            submenu.style.opacity = '0';
+        } else {
+            submenu.style.transform = 'translateY(0%)';
+            submenu.style.opacity = '1';
+        }
+    });
+});
+
+
